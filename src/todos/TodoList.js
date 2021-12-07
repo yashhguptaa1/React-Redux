b52174971787385c75c984ea32930aaced5d931a
Yash Gupta <yashhguptaa1@gmail.com>
2021-12-07 11:16:11 +0530

    Connecting components to store

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import NewTodoForm from './NewTodoForm';
-import TodoListItem from './TodoListItem';
-import './TodoList.css';
+import React from "react";
+import NewTodoForm from "./NewTodoForm";
+import TodoListItem from "./TodoListItem";
 
-const TodoList = ({ todos = [] }) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} />)}
-    </div>
+import { connect } from "react-redux";
+import { removeTodo } from "../redux/actions";
+
+import "./TodoList.css";
+
+const TodoList = ({ todos = [], onRemovePressed }) => (
+  <div className="list-wrapper">
+    <NewTodoForm />
+    {todos.map((todo) => (
+      <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />
+    ))}
+  </div>
 );
 
-export default TodoList;
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
