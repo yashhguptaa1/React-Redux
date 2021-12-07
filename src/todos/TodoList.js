d655ff5b62e022f0724fe2c81ee5ed8ef5ec5fad
Yash Gupta <yashhguptaa1@gmail.com>
2021-12-07 11:53:53 +0530

    Adding complete redux flow

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -3,15 +3,19 @@ import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./TodoListItem";
 
 import { connect } from "react-redux";
-import { removeTodo } from "../redux/actions";
+import { removeTodo, markTodoAsCompleted } from "../redux/actions";
 
 import "./TodoList.css";
 
-const TodoList = ({ todos = [], onRemovePressed }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
   <div className="list-wrapper">
     <NewTodoForm />
     {todos.map((todo) => (
-      <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />
+      <TodoListItem
+        todo={todo}
+        onRemovePressed={onRemovePressed}
+        onCompletedPressed={onCompletedPressed}
+      />
     ))}
   </div>
 );
@@ -22,6 +26,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(removeTodo(text)),
+  onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
