a1851e23386d8f1759a2bfa6dfa211a078683077
Yash Gupta <yashhguptaa1@gmail.com>
2021-12-07 13:58:28 +0530

    Creating redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers ,applyMiddleware} from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -4,17 +4,17 @@ import TodoListItem from "./TodoListItem";
 
 import { connect } from "react-redux";
 import { removeTodo, markTodoAsCompleted } from "../redux/actions";
-
+import { displayAlert } from "../redux/thunks";
 import "./TodoList.css";
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed,onDisplayAlertClicked }) => (
   <div className="list-wrapper">
     <NewTodoForm />
     {todos.map((todo) => (
       <TodoListItem
         todo={todo}
         onRemovePressed={onRemovePressed}
-        onCompletedPressed={onCompletedPressed}
+        onCompletedPressed={onDisplayAlertClicked}
       />
     ))}
   </div>
@@ -27,6 +27,7 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(removeTodo(text)),
   onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
+  onDisplayAlertClicked : (text) => dispatch(displayAlert(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
